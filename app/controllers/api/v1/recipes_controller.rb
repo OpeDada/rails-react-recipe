5182cc05c465023fd7e035c79a56b427d554cbce
OpeDada <opedada@gmail.com>
2021-12-16 14:48:13 +0900

    populate actions in recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,47 @@
 class Api::V1::RecipesController < ApplicationController
-  def index
-  end
 
-  def create
+  def index
+    @recipes = Recipe.all.order(created_at: :desc)
+    render json: @recipes
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
+    render json: @recipe.errors
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      render json: @recipe, status: :created
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      render json: @recipe
+    else
+      render json: @recipe.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    render json: { message: 'Recipe deleted!' }
+  end
+
+  private
+
+  # def set_recipe
+  #   @recipe = Recipe.find(params[:id])
+  # end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :instruction, :photo)
   end
 end
