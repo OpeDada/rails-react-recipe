7977ac3e13f97158f4b6f031b40405805bf20147
OpeDada <opedada@gmail.com>
2021-12-17 01:26:09 +0900

    add recipes to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+puts "Destroying everything - waaaaaaaaa!"
+# PRODUCT
+Recipe.destroy_all
+6.times do |i|
+  recipe = Recipe.create(
+    name: "Recipe #{i + 1}",
+    ingredients: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+    instruction: 'In a medium saucepan, stir the clotted cream, butter, and cornflour over a low-ish heat and bring to a low simmer. Turn off the heat and keep warm.'
+  )
+  recipe.photo.attach(
+    io: File.open(File.join(Rails.root, 'app/assets/images/recipe.jpg')),
+    filename: 'recipe.jpg',
+    content_type: 'image/jpg'
+  )
+end
+
+puts "\nTotal recipes: #{Recipe.all.count}"
