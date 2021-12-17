94e1e08c1504e6fede5ae902be2e917a6d22bc99
OpeDada <opedada@gmail.com>
2021-12-17 14:35:14 +0900

    populate all recipes

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import NewRecipe from "./NewRecipe";
+
+const initialFormState = {
+  name: "",
+  ingredients: "",
+  instruction: "",
+};
 
 const Recipes = (props) => {
   useEffect(() => {
@@ -8,12 +15,35 @@ const Recipes = (props) => {
 
   const [recipes, setRecipes] = useState([]);
 
+  const addRecipe = (recipe) => {
+    const qs = require("qs");
+    axios
+      .post(
+        "/api/v1/recipes",
+        qs.stringify({
+          recipe: {
+            name: recipe.name,
+            ingredients: recipe.ingredients,
+            instruction: recipe.instruction,
+          },
+        })
+      )
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+
+    setRecipes([...recipes, recipe]);
+  };
+
   return (
     <div>
+      <div>
+        <NewRecipe addRecipe={addRecipe} initialFormState={initialFormState} />
+      </div>
       <div className="recipes-list">
         {recipes.map((recipe, index) => (
           <div key={index}>
             {recipe.name}
+            <img src={recipe.image} alt="Recipe pic" />
             {recipe.ingredients}
             {recipe.instruction}
           </div>
