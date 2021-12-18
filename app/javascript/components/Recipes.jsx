3473e7d6c3353a168711043e9982fa19c16cd844
OpeDada <opedada@gmail.com>
2021-12-18 23:39:20 +0900

    change url to json

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -10,7 +10,7 @@ class Recipes extends React.Component {
   }
 
   componentDidMount() {
-    const url = "api/v1/recipes/index";
+    const url = "api/v1/recipes.json";
     fetch(url)
       .then((response) => {
         if (response.ok) {
