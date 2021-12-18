55ef796ecc59ee9942984587385527c6eddb34cb
OpeDada <opedada@gmail.com>
2021-12-18 23:39:53 +0900

    import all recipes to routes

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
-// import Products from "../components/Products";
+import Recipes from "../components/Recipes";
 
 export default (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/products" element={<Products />} /> */}
+      <Route path="/recipes" element={<Recipes />} />
     </Routes>
   </Router>
 );
