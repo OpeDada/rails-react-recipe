c20c3f621d15c59405f20bb10475a8a57cb6b892
OpeDada <opedada@gmail.com>
2021-12-21 23:50:49 +0900

    edit show action

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all.order(created_at: :desc)
-    render json: @recipes
+    @recipe = Recipe.all.order(created_at: :desc)
+    render json: @recipe
   end
 
   def show
     @recipe = Recipe.find(params[:id])
-    render json: @recipe
+    if @recipe
+      render json: @recipe
+    else
+      render json: @recipe.errors
+    end
   end
 
   def create
