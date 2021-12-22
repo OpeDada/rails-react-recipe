c39ec0ab80981424392ad61168c6cb67c984ad7f
OpeDada <opedada@gmail.com>
2021-12-22 14:49:13 +0900

    import new recipe functional file

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
 import Recipe from "../components/Recipe";
-import NewRecipe from "../components/NewRecipe";
+// import NewRecipe from "../components/NewRecipe";
+import NewRecipe from "../components/NewRecipeFunctional";
 
 export default (
   <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recipes" element={<Recipes />} />
-      <Route path="/recipe/:id" element={<Recipe />} />
-      <Route path="/recipe" element={<NewRecipe />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/recipes" element={<Recipes />} />
+      <Route exact path="/recipe/:id" element={<Recipe />} />
+      <Route exact path="/recipe" element={<NewRecipe />} />
     </Routes>
   </Router>
 );
