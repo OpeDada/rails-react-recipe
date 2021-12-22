408df495b15d6723167f1cfa0e121d2461f2153d
OpeDada <opedada@gmail.com>
2021-12-22 23:23:24 +0900

    switch to functional component

--- app/javascript/components/NewRecipe.jsx
+++ app/javascript/components/NewRecipe.jsx
@@ -1,41 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import { withRouter } from "react-router";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-class NewRecipe extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      ingredients: "",
-      instruction: "",
-    };
+const NewRecipe = () => {
+  const [curRecipe, setCurRecipe] = useState({
+    name: "",
+    ingredients: "",
+    instruction: "",
+  });
+  const navigate = useNavigate();
 
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
-  }
+  const onChange = (event) => {
+    curRecipe[event.target.name] = event.target.value;
+    setCurRecipe(curRecipe);
+  };
 
-  stripHtmlEntities(str) {
-    return String(str).replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  }
-
-  onChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  onSubmit(event) {
+  const onSubmit = (event) => {
     event.preventDefault();
-    const url = "/api/v1/recipes/create";
-    const { name, ingredients, instruction } = this.state;
+    const url = "/api/v1/recipes";
 
-    if (name.length == 0 || ingredients.length == 0 || instruction.length == 0)
+    if (
+      curRecipe.name.length == 0 ||
+      curRecipe.ingredients.length == 0 ||
+      curRecipe.instruction.length == 0
+    )
       return;
 
     const body = {
-      name,
-      ingredients,
-      instruction: instruction.replace(/\n/g, "<br> <br>"),
+      name: curRecipe.name,
+      ingredients: curRecipe.ingredients,
+      instruction: curRecipe.instruction.replace(/\n/g, "<br> <br>"),
     };
 
     const token = document.querySelector('meta[name="csrf-token"]').content;
@@ -54,68 +47,64 @@ class NewRecipe extends React.Component {
         throw new Error("Network response was not ok.");
       })
       .then((response) => {
-        console.log(this.props);
-        this.props.history.push(`/recipe/${response.id}`);
+        navigate(`/recipe/${response.id}`);
       })
       .catch((error) => console.log(error.message));
-  }
+  };
 
-  render() {
-    return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-sm-12 col-lg-6 offset-lg-3">
-            <h1 className="font-weight-normal mb-5">
-              Add a new recipe to our awesome recipe collection.
-            </h1>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label htmlFor="recipeName">Recipe name</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="recipeName"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="recipeIngredients">Ingredients</label>
-                <input
-                  type="text"
-                  name="ingredients"
-                  id="recipeIngredients"
-                  className="form-control"
-                  required
-                  onChange={this.onChange}
-                />
-                <small id="ingredientsHelp" className="form-text text-muted">
-                  Separate each ingredient with a comma.
-                </small>
-              </div>
-              <label htmlFor="instruction">Preparation Instructions</label>
-              <textarea
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-sm-12 col-lg-6 offset-lg-3">
+          <h1 className="font-weight-normal mb-5">
+            Add a new recipe to our awesome recipe collection.
+          </h1>
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <label htmlFor="recipeName">Recipe name</label>
+              <input
+                type="text"
+                name="name"
+                id="recipeName"
                 className="form-control"
-                id="instruction"
-                name="instruction"
-                rows="5"
                 required
-                onChange={this.onChange}
+                onChange={onChange}
               />
-              <button type="submit" className="btn custom-button mt-3">
-                Create Recipe
-              </button>
-              <Link to="/recipes" className="btn btn-link mt-3">
-                Back to recipes
-              </Link>
-            </form>
-          </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="recipeIngredients">Ingredients</label>
+              <input
+                type="text"
+                name="ingredients"
+                id="recipeIngredients"
+                className="form-control"
+                required
+                onChange={onChange}
+              />
+              <small id="ingredientsHelp" className="form-text text-muted">
+                Separate each ingredient with a comma.
+              </small>
+            </div>
+            <label htmlFor="instruction">Preparation Instructions</label>
+            <textarea
+              className="form-control"
+              id="instruction"
+              name="instruction"
+              rows="5"
+              required
+              onChange={onChange}
+            />
+            <button type="submit" className="btn custom-button mt-3">
+              Create Recipe
+            </button>
+            <Link to="/recipes" className="btn btn-link mt-3">
+              Back to recipes
+            </Link>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewRecipe;
-// export default withRouter(NewRecipe);
