b9d7574fa4f04f1d992c7d4ae618f4d5f928e6a8
OpeDada <opedada@gmail.com>
2021-12-24 13:45:10 +0900

    update route to match recipe folder

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
-import Recipes from "../components/Recipes";
-import Recipe from "../components/Recipe";
-import NewRecipe from "../components/NewRecipe";
+import Recipes from "../components/recipes/Recipes";
+import Recipe from "../components/recipes/Recipe";
+import NewRecipe from "../components/recipes/NewRecipe";
+// import EditRecipe from "../components/recipes/EditRecipe";
 
 export default (
   <Router>
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/recipes" element={<Recipes />} />
-      <Route exact path="/recipe/:id" element={<Recipe />} />
       <Route exact path="/recipe" element={<NewRecipe />} />
+      <Route exact path="/recipe/:id" element={<Recipe />} />
+      {/* <Route path="/recipes/:id/edit" exact component={EditRecipe} /> */}
     </Routes>
   </Router>
 );
