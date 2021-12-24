9beda7d1c04c015bcf33acb826e0b810aba6a510
OpeDada <opedada@gmail.com>
2021-12-24 13:45:24 +0900

    move files into recipe folder

--- app/javascript/components/NewRecipe.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import RecipeForm from "./RecipeForm";
-
-const NewRecipe = () => {
-  return <RecipeForm />;
-};
-
-export default NewRecipe;
