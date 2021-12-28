40295ccc3ec19f8d90dbd1abee0f46cc1b4ec671
OpeDada <opedada@gmail.com>
2021-12-28 23:40:23 +0900

    remove navigate function

--- app/javascript/components/recipes/RecipesFunctional.jsx
+++ app/javascript/components/recipes/RecipesFunctional.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
-  // const navigate = useNavigate();
 
   useEffect(() => {
     const url = "api/v1/recipes";
